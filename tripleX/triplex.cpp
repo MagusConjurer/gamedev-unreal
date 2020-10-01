86dc1cf4c8cdc5deead3edbb4f5e642d25f5141b
MagusConjurer <cameronisadavis@gmail.com>
2020-09-30 21:27:00 -0600

    Add basic text output

--- tripleX/triplex.cpp
+++ tripleX/triplex.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+
 int main() {
+  std::cout << "You have entered the forest realm." << std::endl;
+  std::cout << "Before you, there are three bags and a pile of small stones." << std::endl;
+  std::cout << "A man steps out from a door in a nearby tree and says:" << std::endl;
+  std::cout << "You must fill the bags with the correct number of stones and place them on the pedestals to proceed into the next realm." << std::endl;
   return 0;
 }
