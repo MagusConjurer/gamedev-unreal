ff1181d623f66c57769b9b0b3b7d6044f6aabed5
MagusConjurer <cameronisadavis@gmail.com>
2020-10-01 19:43:56 -0600

    Add conditional and convert endl to escape newlines

--- tripleX/triplex.cpp
+++ tripleX/triplex.cpp
@@ -2,19 +2,13 @@
 
 int main() {
     // Print the introduction to the game 
-    std::cout << std::endl;
-    std::cout << "==================================" << std::endl;
-    std::cout << std::endl;
-    std::cout << "You have entered the forest realm." << std::endl;
-    std::cout << std::endl;
-    std::cout << "==================================" << std::endl;
-    std::cout << std::endl;
-    std::cout << "Before you, there are three bags and a pile of small stones." << std::endl;
-    std::cout << "A man steps out from a door in a nearby tree and says:" << std::endl;
-    std::cout << "You must fill the bags with the correct number of stones and place them on the pedestals to proceed into the next realm." << std::endl;
-    std::cout << std::endl;
+    std::cout << "==================================\n\n";
+    std::cout << "You have entered the forest realm.\n\n";
+    std::cout << "==================================\n\n";
+    std::cout << "Before you, there are three bags and a pile of small stones.\n";
+    std::cout << "A man steps out from a door in a nearby tree and says:\n";
+    std::cout << "\"You must fill the bags with the correct number of stones and place them on the pedestals to proceed into the next realm.\"\n\n";
 
-    // Declare intial values for the code
     int CodeA = 4;
     int CodeB = 7;
     int CodeC = 3;
@@ -24,18 +18,27 @@ int main() {
 
     int LevelNum = 2 - 1;
 
-    std::cout << "Level " << LevelNum << std::endl;
-    std::cout << "The three weights must add up to: " << CodeSum << std::endl;
-    std::cout << "They must also multiply to equal: " << CodeProduct << std::endl;
-    std::cout << std::endl;
+    // Provide the player with required conditions
+    std::cout << "Level " << LevelNum;
+    std::cout << "\nThe three weights must add up to: " << CodeSum;
+    std::cout << "\nThey must also multiply to equal: " << CodeProduct;
 
     int GuessA, GuessB, GuessC;
 
-    std::cout << "Enter the number of stones you would like to place in each bag: ";
+    std::cout << "\n\nEnter the number of stones you would like to place in each bag: \n";
     std::cin >> GuessA >> GuessB >> GuessC;
 
     int GuessSum = GuessA + GuessB + GuessC;
-    int GuessProduct = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    // Check if the guess is correct
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct) {
+        std::cout << "\nYou placed the correct number of stones in the bags!\n";
+        std::cout << "You proceed to the next room.\n";
+    } else {
+        std::cout << "\nYou placed the incorrect number of stones in the bags!\n";
+        std::cout << "A large stone falls from above, blocking the path forward.\n";
+    }
 
     return 0;
 }
