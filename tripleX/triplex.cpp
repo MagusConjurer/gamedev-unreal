bdfa83f90484102156d1cfb473c2c94182f3be2e
MagusConjurer <cameronisadavis@gmail.com>
2020-10-01 20:45:48 -0600

    Handle invalid user input and add while loop

--- tripleX/triplex.cpp
+++ tripleX/triplex.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 void PrintIntro() {
-    std::cout << "==================================\n\n";
+    std::cout << "\n\n==================================\n\n";
     std::cout << "You have entered the forest realm.\n\n";
     std::cout << "==================================\n\n";
     std::cout << "Before you, there are three bags and a pile of small stones.\n";
@@ -9,8 +9,13 @@ void PrintIntro() {
     std::cout << "\"You must fill the bags with the correct number of stones and place them on the pedestals to proceed into the next realm.\"\n\n";
 }
 
-void PlayGame() {
-    PrintIntro();
+bool PlayGame(int Difficulty) {
+
+    int RoomNum = Difficulty - 1;
+    
+    if (RoomNum == 1) {
+        PrintIntro();
+    }
 
     // Declare code variables
     int CodeA = 4;
@@ -20,10 +25,8 @@ void PlayGame() {
     int CodeSum = CodeA + CodeB + CodeC;
     int CodeProduct = CodeA * CodeB * CodeC;
 
-    int LevelNum = 2 - 1;
-
     // Provide the player with required conditions
-    std::cout << "Level " << LevelNum;
+    std::cout << "Room " << RoomNum;
     std::cout << "\nThe three weights must add up to: " << CodeSum;
     std::cout << "\nThey must also multiply to equal: " << CodeProduct;
 
@@ -39,13 +42,25 @@ void PlayGame() {
     if (GuessSum == CodeSum && GuessProduct == CodeProduct) {
         std::cout << "\nYou placed the correct number of stones in the bags!\n";
         std::cout << "You proceed to the next room.\n";
+        return true;
     } else {
         std::cout << "\nYou placed the incorrect number of stones in the bags!\n";
         std::cout << "A large stone falls from above, blocking the path forward.\n";
+        return false;
     }
 }
 
 int main() {
-    PlayGame();
+    int LevelDifficulty = 2;
+    while (true) {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear();
+        std::cin.ignore();
+
+        if (bLevelComplete) {
+            ++LevelDifficulty;
+        }
+    }   
+    
     return 0;
 }
