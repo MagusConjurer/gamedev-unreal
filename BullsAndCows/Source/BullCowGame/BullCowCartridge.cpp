20a7a3508b0b469d1b271b825b60d0628e87c634
MagusConjurer <cameronisadavis@gmail.com>
2020-10-04 17:16:01 -0600

    Add length check for words and use Printf functionality to pass values into the strings

--- BullsAndCows/Source/BullCowGame/BullCowCartridge.cpp
+++ BullsAndCows/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,13 +4,43 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    InitGame();
+
+    PrintLine(TEXT("The hidden word is: "), *HiddenWord); // Show the word for testing
+    
     PrintLine(TEXT("Welcome to the Bulls and Cows farm."));
-    PrintLine(TEXT("Press Tab, type in START and press Enter to begin"));
+    PrintLine(TEXT("Guess the %i letter word"), HiddenWord.Len());
+    PrintLine(TEXT("Press Tab, type your guess and then press Enter to begin..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("Horde");
-    PrintLine(HiddenWord);
+    if (Input == HiddenWord) {
+        PrintLine(TEXT("That is the correct word!"));
+    } else {
+        PrintLine(TEXT("You have entered the incorrect word."));
+        --Lives;
+        if(Lives == 0) {
+            PrintLine(TEXT("You have run out of lives."));
+            PrintLine(HiddenWord);
+            // Prompt to play again
+        } else {
+            // Check if the input is an isogram
+            // Check if the input is the correct length
+            if(HiddenWord.Len() != Input.Len()) {
+                PrintLine(TEXT("NOTE: Your answer was %i letters, instead of %i."), Input.Len(), HiddenWord.Len());
+            }
+            // Show the new number of Lives
+            PrintLine(TEXT("You have %i lives remaining."), Lives); 
+            // Prompt to enter another word
+        }
+    }
+}
+
+void UBullCowCartridge::InitGame() // Set the starting values to member variables
+{
+    HiddenWord = TEXT("Horde");
+    Lives = 5;
 }
