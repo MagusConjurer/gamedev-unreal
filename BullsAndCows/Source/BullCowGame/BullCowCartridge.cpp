9e6e14160f579e1c8712ddfba04fd263ccfc00f9
MagusConjurer <cameronisadavis@gmail.com>
2020-10-04 15:11:42 -0600

    Generate VS Code project files from UE4 and begin updating the BullCowCartridge.cpp

--- BullsAndCows/Source/BullCowGame/BullCowCartridge.cpp
+++ BullsAndCows/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,13 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to the Bulls and Cows farm."));
+    PrintLine(TEXT("Press Tab, type in START and press Enter to begin"));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
+    FString HiddenWord = TEXT("Horde");
+    PrintLine(HiddenWord);
 }
