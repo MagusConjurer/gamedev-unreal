78db854b5bce1ea4665bbd5079d0f8d0c270784d
MagusConjurer <cameronisadavis@gmail.com>
2020-10-10 19:41:49 -0600

    Add EndGame function and game over check

--- BullsAndCows/Source/BullCowGame/BullCowCartridge.cpp
+++ BullsAndCows/Source/BullCowGame/BullCowCartridge.cpp
@@ -7,34 +7,36 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 
     InitGame();
 
-    PrintLine(TEXT("The hidden word is: "), *HiddenWord); // Show the word for testing
-    
-    PrintLine(TEXT("Welcome to the Bulls and Cows farm."));
-    PrintLine(TEXT("Guess the %i letter word"), HiddenWord.Len());
-    PrintLine(TEXT("Press Tab, type your guess and then press Enter to begin..."));
+    PrintLine(TEXT("The hidden word is: "), *HiddenWord); // Showing the word for testing
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    ClearScreen();
-    if (Input == HiddenWord) {
-        PrintLine(TEXT("That is the correct word!"));
-    } else {
-        PrintLine(TEXT("You have entered the incorrect word."));
-        --Lives;
-        if(Lives == 0) {
-            PrintLine(TEXT("You have run out of lives."));
-            PrintLine(HiddenWord);
-            // Prompt to play again
+    // Check whether the game is still in play
+    if (bGameOver) { 
+        ClearScreen();
+        InitGame();
+    } else { // Check PlayerGuess
+        if (Input == HiddenWord) {
+            PrintLine(TEXT("That is the correct word!"));
+            EndGame();
         } else {
-            // Check if the input is an isogram
-            // Check if the input is the correct length
-            if(HiddenWord.Len() != Input.Len()) {
-                PrintLine(TEXT("NOTE: Your answer was %i letters, instead of %i."), Input.Len(), HiddenWord.Len());
+            PrintLine(TEXT("You have entered the incorrect word."));
+            --Lives;
+            if (Lives == 0) {
+                PrintLine(TEXT("You have run out of lives."));
+                PrintLine(TEXT("The hidden word was: %d"), *HiddenWord);
+                EndGame();
+            } else {
+                // Check if the input is an isogram
+                // Check if the input is the correct length
+                if(HiddenWord.Len() != Input.Len()) {
+                    PrintLine(TEXT("NOTE: Your answer was %i letters \ninstead of %i."), Input.Len(), HiddenWord.Len());
+                }
+                // Show the new number of Lives
+                PrintLine(TEXT("You have %i lives remaining."), Lives); 
+                // Prompt to enter another word
             }
-            // Show the new number of Lives
-            PrintLine(TEXT("You have %i lives remaining."), Lives); 
-            // Prompt to enter another word
         }
     }
 }
@@ -43,4 +45,16 @@ void UBullCowCartridge::InitGame() // Set the starting values to member variable
 {
     HiddenWord = TEXT("Horde");
     Lives = 5;
+    bGameOver = false;
+
+    PrintLine(TEXT("Welcome to the Bulls and Cows farm."));
+    PrintLine(TEXT("Guess the %i letter word"), HiddenWord.Len());
+    PrintLine(TEXT("You have %i lives."), Lives);
+    PrintLine(TEXT("Press Tab, type your guess and then press Enter to begin..."));
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    PrintLine(TEXT("Press Enter to play again."));
 }
