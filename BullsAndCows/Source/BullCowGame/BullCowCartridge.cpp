8862cfd55560326a6c84cc411cb0196ef471c11d
MagusConjurer <cameronisadavis@gmail.com>
2020-12-17 16:33:16 -0700

    Add early returns to remove nesting if/else statements

--- BullsAndCows/Source/BullCowGame/BullCowCartridge.cpp
+++ BullsAndCows/Source/BullCowGame/BullCowCartridge.cpp
@@ -36,29 +36,40 @@ void UBullCowCartridge::InitGame() // Set the starting values to member variable
 void UBullCowCartridge::EndGame()
 {
     bGameOver = true;
-    PrintLine(TEXT("Press Enter to play again."));
+    PrintLine(TEXT("\nPress Enter to play again."));
 }
 
 void UBullCowCartridge::ProcessGuess(const FString Guess)
 {
+    // Check if they have entered the correct word
     if (Guess == HiddenWord) {
+        ClearScreen();
         PrintLine(TEXT("That is the correct word!"));
         EndGame();
-    } else {
-        PrintLine(TEXT("You have entered the incorrect word."));
-        --Lives;
-        if (Lives == 0) {
-            PrintLine(TEXT("You have run out of lives."));
-            PrintLine(TEXT("The hidden word was: %s"), *HiddenWord);
-            EndGame();
-        } else {
-            // Check if the guess is an isogram
-            // Check if the guess is the correct length
-            if(HiddenWord.Len() != Guess.Len()) {
-                PrintLine(TEXT("NOTE: Your answer was %i letters \ninstead of %i."), Guess.Len(), HiddenWord.Len());
-            }
-            PrintLine(TEXT("You have %i lives remaining."), Lives); 
-            PrintLine(TEXT("Enter another word."));
-        }
+        return;
+    } 
+
+    PrintLine(TEXT("You have not entered the correct word."));
+    --Lives;
+
+    // Check if the player has lives remaining
+    if (Lives == 0) {
+        ClearScreen();
+        PrintLine(TEXT("You have run out of lives."));
+        PrintLine(TEXT("The hidden word was: %s"), *HiddenWord);
+        EndGame();
+        return;
+    } 
+
+    //// Check if the guess is an isogram
+    //if (!IsIsogram) {
+    //    PrintLine(TEXT("NOTE: The hidden word has no repeating letters."));
+    //}
+
+    // Check if the guess is the correct length
+    if (HiddenWord.Len() != Guess.Len()) {
+        PrintLine(TEXT("NOTE: Your answer was %i letters \ninstead of %i."), Guess.Len(), HiddenWord.Len());
     }
+    PrintLine(TEXT("You have %i lives remaining."), Lives); 
+    PrintLine(TEXT("Enter another word."));
 }
