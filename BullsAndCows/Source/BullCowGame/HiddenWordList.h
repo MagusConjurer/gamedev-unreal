b88560802a500ee16abed9e9f698a13635625fae
MagusConjurer <cameronisadavis@gmail.com>
2020-12-22 12:16:51 -0700

    Create GetValidWords function to return an array of words that meet the game criteria

--- BullsAndCows/Source/BullCowGame/HiddenWordList.h
+++ BullsAndCows/Source/BullCowGame/HiddenWordList.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "CoreMinimal.h"
 
 const TArray<FString> Words = 
@@ -6,5 +7,20 @@ const TArray<FString> Words =
 	TEXT("fight"),
 	TEXT("human"),
 	TEXT("kings"),
+	TEXT("alliance"),
+	TEXT("azeroth"),
+	TEXT("northrend"),
+	TEXT("conquest"),
+	TEXT("attack"),
+	TEXT("defense"),
+	TEXT("farming"),
+	TEXT("hunting"),
+	TEXT("guild"),
+	TEXT("kalimdor"),
+	TEXT("shadow"),
+	TEXT("kelthuzad"),
+	TEXT("lich"),
+	TEXT("wrath"),
+	TEXT("legion"),
 	TEXT("myths")
 };
