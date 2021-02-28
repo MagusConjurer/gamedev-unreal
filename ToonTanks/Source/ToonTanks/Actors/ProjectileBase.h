bead596d40f6bd073b8cba40f994f2d7aedb62aa
MagusConjurer <cameronisadavis@gmail.com>
2021-02-28 16:26:47 -0700

    Add ProjectileBase class and blueprint

---
+++ ToonTanks/Source/ToonTanks/Actors/ProjectileBase.h
@@ -0,0 +1,36 @@
+// Copyright Cameron Davis 2021 All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ProjectileBase.generated.h"
+
+class UProjectileMovementComponent;
+
+UCLASS()
+class TOONTANKS_API AProjectileBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AProjectileBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UProjectileMovementComponent* ProjectileMovement;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* ProjectileMesh;
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	TSubclassOf<UDamageType> DamageType;
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float MovementSpeed = 1300;
+	UPROPERTY(EditAnywhere, Category = "Damage")
+	float Damage = 50;
+
+};
