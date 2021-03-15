77f7df3058de7cdabf0b1b9dbae967c7e879e48c
MagusConjurer <cameronisadavis@gmail.com>
2021-03-15 16:42:00 -0600

    Add an AIController and setup its basic movement

---
+++ SimpleShooter/Source/SimpleShooter/AIControllers/ShooterAIController.cpp
@@ -0,0 +1,29 @@
+// Copyright Cameron Davis 2021. All Rights Reserved.
+
+
+#include "ShooterAIController.h"
+#include "Kismet/GameplayStatics.h"
+
+void AShooterAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void AShooterAIController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+	if (LineOfSightTo(PlayerPawn))
+	{
+		SetFocus(PlayerPawn);
+		MoveToActor(PlayerPawn, AcceptanceRadius);
+	}
+	else
+	{
+		ClearFocus(EAIFocusPriority::Gameplay);
+		StopMovement();
+	}
+}
---
+++ SimpleShooter/Source/SimpleShooter/AIControllers/ShooterAIController.h
@@ -0,0 +1,23 @@
+// Copyright Cameron Davis 2021. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ShooterAIController.generated.h"
+
+UCLASS()
+class SIMPLESHOOTER_API AShooterAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Tick(float DeltaSeconds) override;
+
+protected:
+	virtual void BeginPlay() override;
+	
+private:
+	UPROPERTY(EditAnywhere)
+	float AcceptanceRadius = 500.f;
+};
